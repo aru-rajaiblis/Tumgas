b06afb83b7dc55c811a7d10704df4bbae1f3ee5f
Aru-RajaIblis <marungfikartsani@gmail.com>
2026-03-20 00:36:46 +0800

    bereskan kode

---
+++ refre.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int pilihan;
+    int saldo = 100000;
+    int nominal;
+
+    printf("Selamat datang di ATM ABC");
+    printf("Silahkan pilih menu dibawah ini\n");
+        
+    while (saldo >= 0)
+    {
+        printf("-----------------------------");
+        printf("| 1. Narik Uang             |");
+        printf("| 2. Tarik Uang             |");
+        printf("| 3. Stop                   |");
+        printf("-----------------------------");
+
+        if (pilihan = 1) {
+            printf("Total saldo kamu: %i", saldo);
+        } else if (pilihan = 2) {
+            printf("Grade: B\n");
+        } else if (pilihan = 3) {
+            printf("Grade: B\n");
+        } else {
+            printf("Grade: C\n");
+        }
+        
+    }
+    
+    
+    return 0;
+}
